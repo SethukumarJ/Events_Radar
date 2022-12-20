1b7b1d0be797d2849ad5d012784a0e00c617e6aa
SethukumarJ <sethukumarj.76@gmail.com>
2022-12-20 09:56:47 +0530

    configured the credentials for sending mail

--- config/mail.config.go
+++ config/mail.config.go
@@ -1,1 +1,49 @@
 package config
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/smtp"
+	"os"
+)
+
+type MailConfig interface {
+	SendMail(to string, message string) error
+}
+
+type mailConfig struct{}
+
+func NewMailConfig() MailConfig {
+	return &mailConfig{}
+}
+
+func (c *mailConfig) SendMail(to string, message string) error {
+
+	fmt.Println("send email to: ", to)
+
+	log.Println("Email Id to send message : ", to)
+	userName := os.Getenv("SMTP_USERNAME")
+	password := os.Getenv("SMTP_PASSWORD")
+	smtpHost := os.Getenv("SMTP_HOST")
+	smtpPort := os.Getenv("SMTP_PORT")
+	fmt.Println(userName)
+	auth := smtp.PlainAuth("", userName, password, smtpHost)
+
+	headers := make(map[string]string)
+
+	headers["Subject"] = "Events Radar Verification Code"
+	headers["From"] = userName
+
+	var msg bytes.Buffer
+	for k, v := range headers {
+		msg.WriteString(k + ": " + v + "\r\n")
+	}
+
+	msg.WriteString("\r\n")
+	msg.WriteString(message)
+
+	// sending email
+	return smtp.SendMail(smtpHost+":"+smtpPort, auth, userName, []string{to}, msg.Bytes())
+
+}
