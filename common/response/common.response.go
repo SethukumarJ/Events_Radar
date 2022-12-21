00b65e71bf08a527e34273506183d93e54ba8597
SethukumarJ <sethukumarj.76@gmail.com>
2022-12-21 14:25:44 +0530

    error respose funtion added

--- common/response/common.response.go
+++ common/response/common.response.go
@@ -1,8 +1,8 @@
 package response
 
+import "strings"
 
-
-//Response is used for static shape json return
+// Response is used for static shape json return
 type Response struct {
 	Status  bool        `json:"status"`
 	Message string      `json:"message"`
@@ -10,12 +10,24 @@ type Response struct {
 	Data    interface{} `json:"data,omitempty"`
 }
 
-//SuccessResponse method is used to inject data value to dynamic success response
+// SuccessResponse method is used to inject data value to dynamic success response
 func SuccessResponse(status bool, message string, data interface{}) Response {
 	res := Response{
 		Status:  status,
 		Message: message,
 		Errors:  nil,
+		Data:    data,
+	}
+	return res
+}
+
+// ErrorResponse method is used to inject data value to dynamic failed response
+func ErrorResponse(message string, err string, data interface{}) Response {
+	splittedError := strings.Split(err, "\n")
+	res := Response{
+		Status:  false,
+		Message: message,
+		Errors:  splittedError,
 		Data:    data,
 	}
 	return res
