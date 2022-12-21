60d0f1361da455c4d98ed07634ddf4d76ecf0e1d
SethukumarJ <sethukumarj.76@gmail.com>
2022-12-21 14:24:24 +0530

    successresponse function added

--- common/response/common.response.go
+++ common/response/common.response.go
@@ -1,1 +1,22 @@
 package response
+
+
+
+//Response is used for static shape json return
+type Response struct {
+	Status  bool        `json:"status"`
+	Message string      `json:"message"`
+	Errors  interface{} `json:"errors,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+//SuccessResponse method is used to inject data value to dynamic success response
+func SuccessResponse(status bool, message string, data interface{}) Response {
+	res := Response{
+		Status:  status,
+		Message: message,
+		Errors:  nil,
+		Data:    data,
+	}
+	return res
+}
