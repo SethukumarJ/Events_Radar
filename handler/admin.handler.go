29b1b9212c5ca2846f75bb56a0c7f9e556517cb0
SethukumarJ <sethukumarj.76@gmail.com>
2022-12-21 19:59:33 +0530

    admin handler

--- handler/admin.handler.go
+++ handler/admin.handler.go
@@ -1,1 +1,22 @@
 package handler
+
+import "radar/service"
+
+type AdminHandler interface {
+}
+
+type adminHandler struct {
+	adminService service.AdminService
+	userService  service.UserService
+}
+
+func NewAdminHandler(
+	adminService service.AdminService,
+	userService service.UserService,
+
+) AdminHandler {
+	return &adminHandler{
+		adminService: adminService,
+		userService:  userService,
+	}
+}
