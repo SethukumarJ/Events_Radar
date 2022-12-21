6c4c4a7fdc77eb7046ff6b57da3c223c171d2a09
SethukumarJ <sethukumarj.76@gmail.com>
2022-12-21 21:46:38 +0530

    user routes added

--- routes/user.routes.go
+++ routes/user.routes.go
@@ -1,1 +1,40 @@
 package routes
+
+import (
+	h "radar/handler"
+	m "radar/middleware"
+
+	"github.com/go-chi/chi"
+)
+
+type UserRoute interface {
+	UserRouter(routes chi.Router,
+		authHandler h.AuthHandler,
+		userHandler h.UserHandler,
+		middleware m.Middleware)
+}
+
+type userRoute struct{}
+
+func NewUserRoute() UserRoute {
+	return &userRoute{}
+}
+
+func (r *userRoute) UserRouter(routes chi.Router,
+	authHandler h.AuthHandler,
+	userHandler h.UserHandler,
+	middleware m.Middleware,
+) {
+
+	routes.Post("/user/signup", authHandler.UserSignup())
+	routes.Post("/user/login", authHandler.UserLogin())
+	routes.Post("/user/send/verification", userHandler.SendVerificationMail())
+	routes.Patch("/user/verify/account", userHandler.VerifyAccount())
+
+	routes.Group(func(r chi.Router) {
+		r.Use(middleware.AuthorizeJwt)
+		r.Get("/user/token/refresh", authHandler.UserRefreshToken())
+
+	})
+
+}
