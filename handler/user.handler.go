cbbd22648f0db952b32f56a684f1e7d603362beb
SethukumarJ <sethukumarj.76@gmail.com>
2022-12-21 19:58:24 +0530

    added verify account function

--- handler/user.handler.go
+++ handler/user.handler.go
@@ -5,11 +5,13 @@ import (
 	"radar/common/response"
 	"radar/service"
 	"radar/utils"
+	"strconv"
 )
 
 type UserHandler interface {
-	SendVerificationEmail() http.HandlerFunc
-	VerifyEmail() http.HandlerFunc
+	SendVerificationMail() http.HandlerFunc
+	VerifyAccount() http.HandlerFunc
+
 }
 
 type userHandler struct {
@@ -48,3 +50,23 @@ func (h *userHandler) SendVerificationMail() http.HandlerFunc {
 	}
 }
 
+// verifyAccount verifies the account
+
+func (c *userHandler) VerifyAccount() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		email := r.URL.Query().Get("Email")
+		code, _ := strconv.Atoi(r.URL.Query().Get("Code"))
+
+		err := c.userService.VerifyAccount(email, code)
+
+		if err != nil {
+			response := response.ErrorResponse("Verification failed, Invalid OTP", err.Error(), nil)
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadRequest)
+			utils.ResponseJSON(w, response)
+			return
+		}
+		response := response.SuccessResponse(true, "Account verified successfully", email)
+		utils.ResponseJSON(w, response)
+	}
+}
